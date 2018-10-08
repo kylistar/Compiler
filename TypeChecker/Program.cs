676da7f67819cee2fab267d63389ebf658d778ba
Emil Orefors <38922220+kylistar@users.noreply.github.com>
2018-10-08 10:22:07 +0200

    Add files via upload

---
+++ TypeChecker/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using Parser;
+using Evaluator;
+using System.Diagnostics;
+
+namespace TypeChecker
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage; {0} [-t | <filename>]", Process.GetCurrentProcess().ProcessName);
+                return;
+            }
+
+            try
+            {
+                StreamReader input;
+
+                if (args[0] == "-t")
+                {
+                    input = new StreamReader(Console.OpenStandardInput());
+                }
+                else
+                {
+                    input = new StreamReader(args[0]);
+                }
+                var program = input.ReadToEnd();
+
+
+                byte[] data = Encoding.ASCII.GetBytes(program);
+                MemoryStream stream = new MemoryStream(data, 0, data.Length);
+                Scanner l = new Scanner(stream);
+                Parser.Parser p = new Parser.Parser(l);
+
+                if (!p.Parse())
+                {
+                    Console.WriteLine("Parse error");
+                    Console.WriteLine(program);
+                    return;
+                }
+
+                try
+                {
+                ProgramTypeChecker stc = new ProgramTypeChecker(p);
+                Console.WriteLine("pass");
+                }
+                catch (TypeCheckerException e)
+                {
+                    Console.WriteLine("{0}", e.Message);
+                    return;
+                }
+}
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+}
+
+    }
+}
